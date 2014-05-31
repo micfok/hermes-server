d460882a691c2a44b05381c39436b9f2268bf6c8
Michael <michaelmfok@gmail.com>
2014-05-31 14:48:20 -0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HermesServer::Application.config.secret_key_base = '9d16fb8702080409c609a5b31e4309a023082aa723954394e17a36b8bc9a8f384f36b6f1d4f626acf64c650e14223ed6fd3b16c25662fcbb904050a978748bbc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HermesServer::Application.config.session_store :cookie_store, key: '_hermes-server_session'
