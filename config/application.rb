316caca3d765bffc36c1953be9bf9eb8a3a40f1f
Michael <michaelmfok@gmail.com>
2014-05-31 14:53:07 -0400

    removed generators

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module HermesServer
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators.view_specs = false
+    config.generators.controller_specs = false
+    config.generators.request_specs = false
+    config.generators.routing_specs = false
   end
 end
